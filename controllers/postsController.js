7a3c062f45d7beb7907b53d2180e375766b830ce
GitDarren <darren321@gmail.com>
2018-04-13 21:35:24 -0500

    updated routes with new Tag model name

---
+++ controllers/postsController.js
@@ -0,0 +1,58 @@
+const db = require("../models");
+
+// Defining methods for postsController
+module.exports = {
+  findAll: function(req, res) {
+    db.posts.findAll().then(result => {
+      console.log("We got all the posts!!!!!");
+      console.log(result);
+      res.send(result);
+    });
+  },
+  findById: function(req, res) {
+    db.posts.findById(req.query.id).then(result => {
+      res.send(result);
+    });
+  },
+  create: function(req, res) {
+    console.log(req.body);
+    db.posts
+      .create({
+        title: req.body.title,
+        slug: req.body.slug,
+        content: req.body.content
+      })
+      .then(() => res.send("success"))
+      .catch(err => {
+        console.log(err);
+        res.status(422).send("fail");
+      });
+  },
+  update: function(req, res) {
+    db.posts
+      .update(
+        {
+          title: req.body.title,
+          slug: req.body.slug,
+          content: req.body.content
+        },
+        { where: { id: req.params.id } }
+      )
+      .then(() => res.send("success"))
+      .catch(err => {
+        console.log(err);
+        res.status(422).send("fail");
+        res.send(result);
+      });
+  },
+  remove: function(req, res) {
+    db.posts
+      .remove({ id: req.params.id })
+      .then(() => response.send("success"))
+      .catch(err => {
+        console.log(err);
+        response.status(422).send("fail");
+        res.send(result);
+      });
+  }
+};
